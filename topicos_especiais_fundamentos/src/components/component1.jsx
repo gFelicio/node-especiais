7b8ba211bd3af01a695d7f3262a3b71dff73df80
gFelicio <gustavo.a.felicio@gmail.com>
2022-02-19 15:45:28 +0000

    react component init

---
+++ topicos_especiais_fundamentos/src/components/component1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// function func1 () {
+//     return <h1>Componente Um</h1>;
+// }
+
+// export default func1;
+
+// shorthand component
+export default () => <h1>Componente Arrow Function</h1>;
+
+// pode ser passado outras coisas além de um comp
+// export default () => (10+10);
---
+++ topicos_especiais_fundamentos/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Component1 from './components/component1';
+
+const element = document.getElementById('root');
+
+ReactDOM.render(
+    <div>
+        <Component1/>
+    </div>
+    , element
+)
